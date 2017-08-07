49050cb463a479fd421d19024a34bb39c0e9e276
froy001 <davidfradis@gmail.com>
2017-08-08 02:02:52 +0300

    initial commit

---
+++ src/main/java/runclass/IsAppliable.java
@@ -0,0 +1,11 @@
+package runclass;
+
+import org.apache.commons.collections4.Predicate;
+
+public class IsAppliable implements Predicate<Score> {
+
+    public boolean evaluate(Score score) {
+        return score.isAppliable();
+    }
+
+}
